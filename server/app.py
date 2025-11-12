b64f0d971d0fcb9dbc354a185df49eef546cfd62
meraghon <163075785+meraghon@users.noreply.github.com>
2025-11-12 14:45:17 -0500

    Update app

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
+from flask_cors import CORS
+import os
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
+
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # API route (test)
 @app.route("/api/hello")
@@ -14,4 +18,5 @@ def serve_index():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("FLASK_PORT", 5000))
     app.run(host="0.0.0.0", port=5000, debug=True)
